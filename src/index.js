f7f5e84077b470eac7cdb1aecc0c8389f2f0f2e3
rijasm09@gmail.com <rijasm09@gmil.com>
2023-06-24 12:37:46 +0530

    returned hardcoded data from component ExpenseItem.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+// if it is a css file we have to add .css
 import App from './App';
+// if it is js file or third party module we dont have to add .js 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
